a2a629ebe93782a869bae0e88040dd5f96c05cd2
Peter Kaminski <kaminski@istori.com>
2023-02-25 15:54:55 -0800

    swap in debugging statements

--- markdown-parkour.py
+++ markdown-parkour.py
@@ -33,21 +33,21 @@ profile = {}
 
 # search for next element of type='type', and optionally, content matching 'content'
 def get_next(elements, type, content=None):
-    print(f"searching for a '{type}' element with content containing '{content}'")
+    logging.debug(f"searching for a '{type}' element with content containing '{content}'")
     while elements:
         next = elements.pop(0)
         if next['type'] == type and (not content or content==get_content(next)):
-            print(f"found '{type}' with content matching '{content}'")
+            logging.debug(f"found '{type}' with content matching '{content}'")
             return elements, next
     return [], None
 
 # search for heading with 'level', and optionally, content matching 'content'
 def get_next_heading(elements, level, content=None):
-    print(f"searching for H{level} with content containing '{content}'")
+    logging.debug(f"searching for H{level} with content containing '{content}'")
     while elements:
         elements, heading = get_next(elements, HEADING)
         if heading and heading[LEVEL] == level and (not content or content==get_content(heading)):
-            print(f"found H{level} with content matching '{content}'")
+            logging.debug(f"found H{level} with content matching '{content}'")
             return elements, heading
     raise ParseError(f"Heading level {level} not found")
 
@@ -67,7 +67,7 @@ def parse_profile(ast):
     # get name from H1
     elements, heading = get_next_heading(elements, 1)
     profile['name'] = get_content(heading)
-    print(f"name: {profile['name']}")
+    logging.info(f"name: {profile['name']}")
 
     # check first paragraph for page type
     elements, paragraph = get_next(elements, PARAGRAPH)
@@ -78,27 +78,27 @@ def parse_profile(ast):
     elements, heading = get_next_heading(elements, 2, 'My current tools and practices')
 
     # find a list
-    print("\n\ntools or practices")
+    logging.info("\n\ntools or practices")
     elements, list = get_next(elements, LIST)
     listitems = list[CHILDREN]
     while listitems:
         listitems, listitem = get_next(listitems, LISTITEM)
-        print(f"tool or practice sentence: {get_listitem_content(listitem)}")
+        logging.info(f"tool or practice sentence: {get_listitem_content(listitem)}")
 
     # find specific H2 heading
     elements, heading = get_next_heading(elements, 2, 'Thinking Tool Ratings')
     
     # find a list
-    print("\n\ntools and ratings")
+    logging.info("\n\ntools and ratings")
     elements, list = get_next(elements, LIST)
     tools = list[CHILDREN]
     while tools:
         tools, tool = get_next(tools, LISTITEM)
-        print(f"tool string: {get_listitem_content(tool)}")
+        logging.info(f"tool string: {get_listitem_content(tool)}")
         ratings = get_listitem_sublist(tool)[CHILDREN]
         while ratings:
             ratings, rating = get_next(ratings, LISTITEM)
-            print(f"rating string: {get_listitem_content(rating)}")
+            logging.info(f"rating string: {get_listitem_content(rating)}")
 
 def main():
     try:
